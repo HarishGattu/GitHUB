1458f688b31b43c2719d8a28e9f854544c16920b
SOGR <SOGR@192.168.0.4>
2022-05-14 13:17:50 +0530

    First project

---
+++ src/git/TC_GIT.java
@@ -0,0 +1,22 @@
+package git;
+
+public class TC_GIT {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	 String hungry="yes";
+	
+	if(hungry=="yes") 
+	{
+	
+		System.out.println("Eat something");
+	}
+else
+{
+	System.out.println("Not hungry");
+
+}
+}
+}
